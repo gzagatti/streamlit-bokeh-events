01b818d69c2286eb9879d641edea62a859c47c7e
Guilherme Zagatti <11043156+gzagatti@users.noreply.github.com>
2025-08-29 17:11:37 +0200

    fix npm compilation, borrow best practices from streamlit/streamlit-bokeh

--- streamlit_bokeh_events/frontend/src/StreamlitBokehEventsComponent.tsx
+++ streamlit_bokeh_events/frontend/src/StreamlitBokehEventsComponent.tsx
@@ -4,7 +4,6 @@ import {
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
-import { embed } from "@bokeh/bokehjs";
 
 interface State {
   eventDetailMap: Map<string, object>;
@@ -47,7 +46,8 @@ class StreamlitBokehEventsComponent extends StreamlitComponentBase<State> {
 
     const bokehJson = this.props.args["bokeh_plot"]
     const figure = JSON.parse(bokehJson)
-    embed.embed_item(figure)
+    const Bokeh = window.Bokeh;
+    Bokeh.embed.embed_item(figure)
     const plot = figure && figure.doc && figure.doc.roots && figure.doc.roots.references
         ? figure.doc.roots.references.find((e: any) => e.type === "Plot")
         : undefined
--- streamlit_bokeh_events/frontend/src/index.tsx
+++ streamlit_bokeh_events/frontend/src/index.tsx
@@ -2,6 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import StreamlitBokehEventsComponent from "./StreamlitBokehEventsComponent"
 
+declare global {
+  interface Window {
+    Bokeh: any
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <StreamlitBokehEventsComponent />
